6dc59ccbb7a664748724e4ad9108486910d966f7
iris <vh.hg@hotmail.com>
2024-08-13 19:54:58 +0800

    add autotag options

--- lua/plugins/nvim-ts-autotag.lua
+++ lua/plugins/nvim-ts-autotag.lua
@@ -1,7 +1,7 @@
 return {
 	"windwp/nvim-ts-autotag",
 	opts = {
-
+		opts = { enable_close = true, enable_rename = true, enable_close_on_slash = false },
 	},
 	config = function(_, opts)
 		require("nvim-ts-autotag").setup(opts)
