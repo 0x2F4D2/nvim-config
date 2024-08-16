e7adbd8e3f9fb6a7647a78a032d8df9bbd0c6e5b
iris <vh.hg@hotmail.com>
2024-08-16 16:18:03 +0800

    use bufferline as tabs

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,29 @@
+-- nvim选项卡插件
+return {
+	"akinsho/bufferline.nvim",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+		options = {
+			mode = "buffer",
+			numbers = "ordinal",
+			close_command = "Bdelete",
+			right_mouse_command = "Bdelete",
+			left_mouse_command = "Bdelete",
+			diagnostics = "nvim_lsp",
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = function()
+						return vim.fn.getcwd()
+					end,
+					text_align = "center",
+					highlight = true,
+					separator = true,
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+	end,
+}
