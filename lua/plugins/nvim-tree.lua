39e51dbe2fff3d5396074f5922b5e5a3740df0bf
iris <vh.hg@hotmail.com>
2024-08-22 16:27:58 +0800

    close last tabs to auto close nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,60 +1,70 @@
 -- File Explorer / Tree
 return {
-  -- https://github.com/nvim-tree/nvim-tree.lua
-  "nvim-tree/nvim-tree.lua",
-  dependencies = {
-    -- https://github.com/nvim-tree/nvim-web-devicons
-    "nvim-tree/nvim-web-devicons", -- Fancy icon support
-  },
-  opts = {
-    -- git
-    git = {
-      enable = true,
-      ignore = false,
-      timeout = 500,
-    },
-    renderer = {
-      highlight_opened_files = "all",
-      indent_markers = {
-        enable = true,
-      },
-    },
-    actions = {
-      open_file = {
-        window_picker = {
-          enable = false,
-        },
-      },
-    },
-    filters = {
-      custom = { '.DS_Store' }
-    }
-  },
+	-- https://github.com/nvim-tree/nvim-tree.lua
+	"nvim-tree/nvim-tree.lua",
+	dependencies = {
+		-- https://github.com/nvim-tree/nvim-web-devicons
+		"nvim-tree/nvim-web-devicons", -- Fancy icon support
+	},
+	opts = {
+		-- git
+		git = {
+			enable = true,
+			ignore = false,
+			timeout = 500,
+		},
+		renderer = {
+			highlight_opened_files = "all",
+			indent_markers = {
+				enable = true,
+			},
+		},
+		actions = {
+			open_file = {
+				window_picker = {
+					enable = false,
+				},
+			},
+		},
+		filters = {
+			custom = { ".DS_Store" },
+		},
+	},
 
 	config = function(_, opts)
-	-- Recommended settings to disable default netrw file explorer
-	vim.g.loaded_netrw = 1
-	vim.g.loaded_netrwPlugin = 1
+		-- Recommended settings to disable default netrw file explorer
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-	-- 配置按键
-	local function my_on_attach(bufnr)
-		local api = require("nvim-tree.api")
-		local map = vim.keymap.set
+		-- 配置按键
+		local function my_on_attach(bufnr)
+			local api = require("nvim-tree.api")
+			local map = vim.keymap.set
 
-		local function tips(desc)
-			return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			local function tips(desc)
+				return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
+
+			api.config.mappings.default_on_attach(bufnr)
+			-- custom mappings
+			map("n", "?", api.tree.toggle_help, tips("Help"))
+			map("n", "v", api.node.open.vertical, tips("Open: Vertical Split"))
+			map("n", "h", api.node.open.horizontal, tips("Open: Vertical Split"))
+			--map("n", "<LeftRelease>", api.node.open.edit, tips("打开Tree")) -- 打开tree
 		end
 
-		api.config.mappings.default_on_attach(bufnr)
-		-- custom mappings
-		map("n", "?", api.tree.toggle_help, tips("Help"))
-		map("n", "v", api.node.open.vertical, tips("Open: Vertical Split"))
-		map("n", "h", api.node.open.horizontal, tips("Open: Vertical Split"))
-		--map("n", "<LeftRelease>", api.node.open.edit, tips("打开Tree")) -- 打开tree
-	end
+		opts.on_attach = my_on_attach
 
-	opts.on_attach = my_on_attach
+		require("nvim-tree").setup(opts)
 
-	require("nvim-tree").setup(opts)
+    -- auto close
+		vim.api.nvim_create_autocmd("BufEnter", {
+			nested = true,
+			callback = function()
+				if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+					vim.cmd("quit")
+				end
+			end,
+		})
 	end,
 }
