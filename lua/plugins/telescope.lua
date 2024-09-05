6b280df2cc9cd03fbdba78dc2503b8242a7b6e04
iris <vh.hg@hotmail.com>
2024-09-06 00:47:14 +0800

    add plugin telescope for code search and file search

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,22 @@
+-- plugins/telescope.lua:
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+		},
+	},
+	opts = {},
+	config = function(_, opts)
+		require("telescope").setup(opts)
+		require("telescope").load_extension("fzf")
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	end,
+}
